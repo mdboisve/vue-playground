549e88645f7a65b5c0bab0ffe8905b490faba447
Mike Boisvert <mdboisve@mtu.edu>
2021-10-10 14:40:38 -0400

    Commit

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+
+};
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
